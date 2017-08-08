cb81d31c9bc852ff652ac61a5b5a3917cefc69e8
Yuki Koya <ykky@pepabo.com>
2017-08-08 20:53:05 +0900

    1段目のサブコマンドでエラーになるのでlazy_defaultもやめる
    default値が初期化されずに呼び出されるっぽい？

--- lib/yao/cli/base.rb
+++ lib/yao/cli/base.rb
@@ -3,12 +3,13 @@ require 'yao/cli/formatter'
 
 module Yao::Cli
   class Base < Thor
-    class_option :format, :type => :string, :lazy_default => "json", :aliases => :f,
+    class_option :format, :type => :string, :aliases => :f,
       :enum => Yao::Cli::Formatter.formats, :desc => "Output format (default: json)"
 
     private
     def pretty_output(obj)
-      puts Yao::Cli::Formatter.get(options[:format]).dump(obj)
+      format = options[:format] || "json"
+      puts Yao::Cli::Formatter.get(format).dump(obj)
     end
 
     def is_uuid?(str)
